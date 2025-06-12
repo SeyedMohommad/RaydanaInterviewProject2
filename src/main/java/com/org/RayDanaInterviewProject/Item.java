8c0cf5b67ff058543d75ae07cecbc8260d5d2b37
SeyedMohammadAminAltolie-Dev <a.amin771116@yahoo.com>
2025-06-12 18:12:42 +0330

    Add Item class to represent accounting document entries
    - Defined Item class with fields: code, type, amount, desc.
    - Added constructor for easy initialization of all fields.

--- src/main/java/com/org/RayDanaInterviewProject/Item.java
+++ src/main/java/com/org/RayDanaInterviewProject/Item.java
@@ -1,4 +1,15 @@
 package com.org.RayDanaInterviewProject;
 
 public class Item {
+    String code;
+    int type;
+    int amount;
+    String desc;
+
+    public Item(String code, int type, int amount, String desc) {
+        this.code = code;
+        this.type = type;
+        this.amount = amount;
+        this.desc = desc;
+    }
 }
