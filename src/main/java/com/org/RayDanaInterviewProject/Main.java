acd88c28e40a9f819f458f94a7dc44b1ce3b342d
SeyedMohammadAminAltolie-Dev <a.amin771116@yahoo.com>
2025-06-12 18:14:03 +0330

    Add Main class for testing accounting document creation and validation
    - Created Main class with sample Item list for manual testing.
    - Demonstrated AccountingDoc usage: printing item status and validation result.
    - Included commented-out section for reading items from CSV using CSVReader.

--- src/main/java/com/org/RayDanaInterviewProject/Main.java
+++ src/main/java/com/org/RayDanaInterviewProject/Main.java
@@ -1,7 +1,34 @@
 package com.org.RayDanaInterviewProject;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(new Item("101", 1, 50000, "بانک بدهکار"));
+        itemList.add(new Item("202", 2, 50000, "درآمد فروش بستانکار"));
+
+        AccountingDoc doc = new AccountingDoc(itemList);
+
+        System.out.println("وضعیت آیتم‌های سند:");
+        doc.printItemsStatus();
+
+        System.out.println("\nبررسی و نتیجه سند:");
+        Result result = doc.createDoc();
+        result.printResult();
+
+
+
+//        List<Item> itemList = CSVReader.readItemsFromCSV("items.csv");
+//
+//        AccountingDoc doc = new AccountingDoc(itemList);
+//
+//        System.out.println("وضعیت آیتم‌های سند:");
+//        doc.printItemsStatus();
+//
+//        System.out.println("\nبررسی و نتیجه سند:");
+//        Result result = doc.createDoc();
+//        result.printResult();
+
     }
 }
